226b85c172257f4339ce68a25d1a9c5a69ae6926
Granddad766 <84017644+Granddad766@users.noreply.github.com>
2021-05-29 00:56:48 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-function compute()
+function compoundInterest(principal, interest, nyear ) 
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years");
-    var interest = principal * years * rate/100;
-    var year = new Date().getFullYear()+parseInt(years);
-    function updateRate()
-    {
-        var rateval = document.getElementById("rate").value;document.getElementById("rate_val").innerText=rateval;
-    }
-    
+
+//Above define function name, { indicates start of script. Below 3 lines. Input 3 values from HTML Form (Number, Range and Select types), assign the values to new variables principal, interest, nyear, values extracted from getPrincipalValue, getInterestRate and getYear
+
+principal = document.getElementById("getPrincipalValue").value;
+interest = document.getElementById("getInterestRate").value;
+nyear = document.getElementById("getYear").value;
+
+//Create two new variables (Var command) assigning the Product of 3 variables above and calling an inbuilt java function to check current date and add the number of years to it, ready for output.
+
+var resultInterest = (principal * interest/100 * nyear);
+var year = new Date().getFullYear()+parseInt(nyear);
+
+//Ouput text and variable values, which are then displayed within the html, inside P tags. Problem unresolved is trying to output product+principal, is being interpreted as a string as as such puts the principal value next to the output value.
+
+document.getElementById("outputPrincipal").innerHTML ="If you deposit? $" + principal;
+document.getElementById("outputInterest").innerHTML = "at an interest rate of " + interest + "%"; 
+document.getElementById("compounded").innerHTML = "You will receive $ " + resultInterest
+document.getElementById("outputYear").innerHTML = "in the year " + year;
 }
-        
